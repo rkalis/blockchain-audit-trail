2ca871d5645552cad89f7bb364a6c9bab9913df8
Rosco Kalis <roscokalis@gmail.com>
2018-04-28 19:31:00 +0200

    Changed the compile contracts.sh script to exit early and to only generate the contracts that have been compiled

--- backend/compile_contracts.sh
+++ backend/compile_contracts.sh
@@ -9,15 +9,55 @@ JAVA_BASE_DIRECTORY="dom/src/main/java"
 CONTRACTS_PACKAGE="org.incode.eurocommercial.contactapp.dom.audit.contracts.generated"
 TRUFFLE_DIR="truffle"
 
-# Compile and deploy (if --deploy flag is set)
-pushd $TRUFFLE_DIR &>/dev/null
-truffle compile
-if [[ $* == *--deploy* ]]; then
-    truffle deploy
-fi
-popd &>/dev/null
+declare -a CONTRACT_FILES=()
 
-# Generate web3j contract objects
-for contract in $TRUFFLE_DIR/build/contracts/*.json; do
-    web3j truffle generate $contract -o $JAVA_BASE_DIRECTORY -p $CONTRACTS_PACKAGE
-done
+main() {
+    pushd $TRUFFLE_DIR &>/dev/null
+    compile
+    deploy
+    popd &>/dev/null
+    generate
+}
+# Compile smart contracts and save the ones that have been compiled
+compile() {
+    OUTPUT=$(truffle compile)
+    EXIT_STATUS=$?
+    echo "${OUTPUT}"
+    add_contract_files "${OUTPUT}"
+    exit_if_failed $EXIT_STATUS
+}
+
+# Deploy if the --deploy flag is set
+deploy() {
+    if [[ $* == *--deploy* ]]; then
+        truffle deploy
+    fi
+    exit_if_failed $?
+}
+
+# Generate web3j contract objects only from the contracts that were compiled
+generate() {
+    for contract in ${CONTRACT_FILES[@]}; do
+        web3j truffle generate $contract -o $JAVA_BASE_DIRECTORY -p $CONTRACTS_PACKAGE
+    done
+}
+
+# Add compiled contract json files to the list to be generated
+add_contract_files() {
+    while read line; do
+        if [[ $line != Compiling* ]]; then
+            continue
+        fi
+        CONTRACT=${line##"Compiling ./contracts/"}
+        CONTRACT=${CONTRACT%%".sol..."}
+        CONTRACT_FILES+=("$TRUFFLE_DIR/build/contracts/$CONTRACT.json")
+    done <<< "$1"
+}
+
+exit_if_failed() {
+    if [[ $1 -ne 0 ]]; then
+        exit $1
+    fi
+}
+
+main "$@"
