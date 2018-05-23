137bac0bb7c25a919355aaaa1fca024f1b75ee62
Rosco Kalis <roscokalis@gmail.com>
2018-05-23 19:27:19 +0200

    Cleaned up Audit Trail contract

--- backend/truffle/contracts/AuditTrail.sol
+++ backend/truffle/contracts/AuditTrail.sol
@@ -11,7 +11,6 @@ contract AuditTrail {
     mapping(bytes28 => bytes32) public transactionHashes;
 
     event Audit(bytes28 transactionIdentifier, bytes32 transactionHash);
-    event Validate(bytes28 transactionIdentifier, bytes32 transactionHash);
 
     modifier ownerOnly {
         require(msg.sender == owner);
@@ -34,74 +33,6 @@ contract AuditTrail {
     }
 
     function validate(bytes28 transactionIdentifier, bytes32 transactionHash) external view returns(uint8) {
-        if (transactionHashes[transactionIdentifier] == transactionHash) {
-            return 0;
-        } else {
-            return 1;
-        }
-        // emit Validate(transactionIdentifier, transactionHash);
+        return transactionHashes[transactionIdentifier] == transactionHash ? 0 : 1;
     }
 }
-
-// pragma solidity 0.4.23;
-
-// contract AuditTrail {
-//     address public owner;
-//     AuditEntry[] public auditEntries;
-
-//     struct AuditEntry {
-//         bytes32 user;
-//         uint256 timestamp;
-//         bytes16 transactionId; // UUID
-//         uint8 sequence;
-//         string targetClass;
-//         string targetStr;
-//         string memberIdentifier;
-//         string propertyId;
-//         string preValue;
-//         string postValue;
-//     }
-
-//     event Audit(
-//         bytes32 user,
-//         uint256 timestamp,
-//         bytes16 transactionId,
-//         uint8 sequence,
-//         string targetClass,
-//         string targetStr,
-//         string memberIdentifier,
-//         string propertyId,
-//         string preValue,
-//         string postValue
-//     );
-
-//     modifier ownerOnly {
-//         require(msg.sender == owner);
-//         _;
-//     }
-
-//     constructor() public {
-//         owner = msg.sender;
-//     }
-
-//     function kill() public ownerOnly {
-//         selfdestruct(owner);
-//     }
-
-//     function audit(
-//         bytes32 user,
-//         uint256 timestamp,
-//         bytes16 transactionId,
-//         uint8 sequence,
-//         string targetClass,
-//         string targetStr,
-//         string memberIdentifier,
-//         string propertyId,
-//         string preValue,
-//         string postValue
-//     ) external ownerOnly {
-//         auditEntries.push(
-//             AuditEntry(user, timestamp, transactionId, sequence, targetClass, targetStr, memberIdentifier, propertyId, preValue, postValue));
-//         emit Audit(user, timestamp, transactionId, sequence, targetClass, targetStr, memberIdentifier, propertyId, preValue, postValue);
-//     }
-// }
