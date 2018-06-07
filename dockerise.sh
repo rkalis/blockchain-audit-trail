91244f871787e334d6ab53872d7069e8588df083
Rosco Kalis <roscokalis@gmail.com>
2018-06-07 15:02:06 +0200

    Fixed Dockerfile

--- dockerise.sh
+++ dockerise.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-mvn clean install && docker build -t rkalis/contactapp . && docker run -it --rm -p 8888:8080 rkalis/contactapp #-p "isis.manifist=1231231;isis.database.driver=sqlServer;"
+mvn clean install -DskipTests && docker build -t rkalis/blockchain-audit-trail . && docker run -it --rm -p 8080:8080 rkalis/blockchain-audit-trail
